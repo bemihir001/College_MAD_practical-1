3a5f25a3111d4f60015db050ec2a15e13d6a6e5a
Mihir Parmar <bemihir123@gmail.com>
2022-08-05 22:21:48 +0530

    Initial commit

---
+++ src/main/kotlin/p1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var a : Int = 22
+    var b = 1.5F
+    var c : Char = 'T'
+    println("Integer Value: $a")
+    println("Float Value: $b")
+    println("Char Value: $c")
+       // println(VARIABLE_NAME :: class.java.typeName) -> return the type of variable
+
+}
